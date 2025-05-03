23fe19e40fba02f2cb042662600c92e985239c21
Santhiya E <santhiyae587@gmail.com>
2025-05-03 13:41:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,6 @@ def index():
         </html>
     ''', table=product_summary.to_html(index=False), pie_chart=pie_chart_html)
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
